a0da7d5112f005449fe8f76443e17640eab27b72
Kishore Prasath <kishoreprasathece@gmail.com>
2024-10-17 18:40:14 +0530

    done

--- TASKF/src/component/Navbar.jsx
+++ TASKF/src/component/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({ cart, setModalOpen }) => {
   return (
 
 <nav className="bg-black p-2">
-  <h1 className="text-white text-center font-rig -my-3 text-3xl">ALPHA LIFE STYLE</h1>
+  <h1 className="text-white text-center font-rig -my-3 text-3xl">E-CART</h1>
   <div className="flex justify-between items-center">
     <div className="flex flex-col items-center ml-auto mx-10">
     <div className="text-red-500 text-2xl bg-opacity-85 "> {cart.length}</div>
